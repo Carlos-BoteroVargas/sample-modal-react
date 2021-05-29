cc847bb21d03a18b6b8a88a2b62d2c4d2da06539
Carlos Botero-Vargas <80601385+Carlos-BoteroVargas@users.noreply.github.com>
2021-05-29 11:58:02 -0500

    Initial commit - working Modal (Camera)

---
+++ src/components/Modal.js
@@ -0,0 +1,126 @@
+import React, { useRef, useEffect, useCallback } from 'react';
+import { useSpring, animated } from 'react-spring';
+import styled from 'styled-components';
+import { MdClose } from 'react-icons/md'
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 11px;
+`;
+
+const ModalImg = styled.img`
+  width: 100%;
+  height: 100%;
+  background: #000;
+  border-radius: 11px 0 0 11px;
+`;
+
+const ModalContent = styled.div`
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+  border-radius: 0px 10px 10px 0;
+  p {
+    margin-bottom: 1rem;
+  }
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
+
+const Modal = ({ showModal, setShowModal}) => {
+  const modalRef = useRef();
+
+  const animation = useSpring({
+    config: {
+      duration: 250
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+  });
+
+  const closeModal = e => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    e => {
+      if (e.key === 'Escape' && showModal) {
+        setShowModal(false);
+        console.log('I pressed');
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(
+    () => {
+      document.addEventListener('keydown', keyPress);
+      return () => document.removeEventListener('keydown', keyPress);
+    },
+    [keyPress]
+  );
+
+  const bgImage = 'https://c0.wallpaperflare.com/preview/967/594/540/black-mamiya-video-camera.jpg'
+
+  return (
+    <>
+      {showModal ? (
+        <Background ref={modalRef} onClick={closeModal}>
+          <animated.div style={animation}>
+            <ModalWrapper showModal={showModal}>
+              <ModalImg src={bgImage} alt='camera'/>
+              <ModalContent>
+                <h1>Are you ready?</h1>
+                <p>Get exclusive access to our next launch.</p>
+                <button>Join Now</button>
+              </ModalContent>
+              <CloseModalButton
+                aria-label='Close modal'
+                onClick={() => setShowModal(prev => !prev)}
+              />
+            </ModalWrapper>
+          </animated.div>
+        </Background>
+      ) : null}
+    </>
+  )
+};
+
+export default Modal;
